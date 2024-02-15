b7bd040b3d2f4678ae2a2eb42c9d2c3b518a9075
emna <emna.jridi@esen.tn>
2024-02-15 15:07:09 +0100

    connection BD

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+require("dotenv").config()
+
 const express = require('express')
 const app = express();
-
+const connectDB = require('./db/connect'); 
 const port = 4000;
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`server is listening on port ${port}`));
     } catch (error) {
         console.log(error);
     }
 }
 start()
+//KbrgA5oAO8MG5NGI
